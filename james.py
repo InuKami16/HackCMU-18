5c5374fab3b0ea5ccdd086e8e08e7258833b773a
jeffreytsaw <31784874+jeffreytsaw@users.noreply.github.com>
2018-09-21 23:00:56 -0400

    Add files via upload

--- james.py
+++ james.py
@@ -3,16 +3,13 @@ def convertSong():
     s = converter.parse('Disney_Themes_-_Whole_New_World.mid')
     count = 0
     for el in s.recurse():
-        try:
-            lastNote = s.recurse().getElementsByClass('Note')[count]
-            #print (note)
-            #print (note.getContextByClass('Part'))
-            print (type(lastNote.getContextByClass("Part")))
-            if (note.getContextByClass("Part") == '0x107da7e48'):
-                el.activeSite.replace(el, instrument.ElectricGuitar())
-                print (1)
-        except:
-            pass
+        #print (el.classes)
+        if 'Piano' in el.classes:
+            el.activeSite.replace(el, instrument.ElectricGuitar())
+            print (1) 
+        elif  'StringInstrument' in el.classes:
+            el.activeSite.replace(el, instrument.Bass())
+            print (2)
         # if 'Instrument' in el.classes: # or 'Piano'
         #     el.activeSite.replace(el, instrument.ElectricGuitar())
         count += 1
